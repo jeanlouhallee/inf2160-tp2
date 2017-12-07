21d9fc0e6598383cd3f4591456cf02a56d6e33ea
unknown <salut.www@hotmail.com>
2017-12-06 22:14:30 -0500

    Ajouter la prise en charge du OU et le filtrage de la liste

--- tp2.pl
+++ tp2.pl
@@ -6,27 +6,48 @@
 % chanson( TitreChanson, IdDisque, [ Caracteristique ], Duree )
 
 % Permet de retourner les informations nécéssaires pour l'affichage à partir d'un titre de chanson.
-donneInfosSelonChansons(TitresChansons, Infos) :- findall([NomGroupe, TitreDisque, TitreChanson, Duree], (chanson(TitreChanson, IdDisque, _, Duree), member(TitreChanson, TitresChansons), disque(IdDisque, IdGroupe, TitreDisque), groupe(IdGroupe, NomGroupe, _)), Infos).
+donneInfosSelonChansons(TitresChansons, Infos) :- findall([NomGroupe, TitreDisque, TitreChanson, Duree], (chanson(TitreChanson, IdDisque, _, Duree), 
+																											member(TitreChanson, TitresChansons), 
+																											disque(IdDisque, IdGroupe, TitreDisque), 
+																											groupe(IdGroupe, NomGroupe, _)), Infos).
 
 % Permet de trouver toutes les chansons d'un gorupe
-chansonSelonGroupe(IdGroupe, TitreChanson) :- groupe(IdGroupe, _, _), disque(IdDisque, IdGroupe, _), chanson(TitreChanson, IdDisque, _, _).
+chansonSelonGroupe(IdGroupe, TitreChanson) :- groupe(IdGroupe, _, _), 
+											disque(IdDisque, IdGroupe, _), 
+											chanson(TitreChanson, IdDisque, _, _).
 
 % Permet de trouver toutes les chansons qui correspondent à une caractéristique.
-chansonSelonCaracteristique(Caracteristique, TitreChanson) :- chanson(TitreChanson,_, LC, _), member(Caracteristique, LC).
+chansonSelonCaracteristique(Caracteristique, TitreChanson) :- chanson(TitreChanson,_, LC, _), 
+															member(Caracteristique, LC).
 
 % Permet de trouver les chansons selon une caractéristique de chanson ET de groupe.
-chansonDeGroupeEtCaracteristique(Caracteristique, TitreChanson) :- chansonSelonCaracteristique(Caracteristique, TitreChanson); (groupe(IdGroupe, _, LC), member(Caracteristique, LC), chansonSelonGroupe(IdGroupe, TitreChanson)).
+chansonDeGroupeEtCaracteristique(Caracteristique, TitreChanson) :- chansonSelonCaracteristique(Caracteristique, TitreChanson);
+																(groupe(IdGroupe, _, LC), member(Caracteristique, LC), 
+																chansonSelonGroupe(IdGroupe, TitreChanson)).
 
 % Permet de trouver les chansons selon plusieurs caractérstiques de chanson ET de groupe.
 chansonDeGroupeEtCaracteristiques([Caracteristique], TitresChansons) :- chansonDeGroupeEtCaracteristique(Caracteristique, TitresChansons).
-chansonDeGroupeEtCaracteristiques([Caracteristique|R], TitresChansons) :- chansonDeGroupeEtCaracteristique(Caracteristique, TitresChansons), chansonDeGroupeEtCaracteristiques(R, TitresChansons).
+chansonDeGroupeEtCaracteristiques([Caracteristique|R], TitresChansons) :- chansonDeGroupeEtCaracteristique(Caracteristique, TitresChansons), 
+																		chansonDeGroupeEtCaracteristiques(R, TitresChansons).
 
 % Permet de retourner une liste de titres de chanson selon plusieurs caractéristiques
 playlistEt(Caracteristiques, TitresChansons) :- findall(Chanson, chansonDeGroupeEtCaracteristiques(Caracteristiques, Chanson), TitresChansons).
 
-% Permet de retourner plusieurs listes de titres de chanson (chaque liste est un ensemble de ET, mais les listes sont séparés car ce sont des OU).
-plusieursPlaylist([Caracteristiques], Playlist) :- playlistEt(Caracteristiques, Playlist), !.
-plusieursPlaylist([Caracteristiques|R], Playlist) :-  playlistEt(Caracteristiques, Playlist); plusieursPlaylist(R, Playlist).
+% Permet de retourner plusieurs listes de titres de chanson (chaque liste est un ensemble de ET, mais les listes sont séparés car ce sont des OU)
+plusieursPlaylist([Caracteristiques | []], Playlist) :- playlistEt(Caracteristiques, Playlist), !.
+plusieursPlaylist([Caracteristiques|R], Playlist) :- playlistEt(Caracteristiques, PlaylistEt),
+													plusieursPlaylist(R, PlaylistAutre),
+													append(PlaylistEt, PlaylistAutre, Playlist).
+										
+										
+trierListe([Titre|Liste], SansDoublons) :- trierListe(Liste, ListeIntermediaire), !,
+											filterListe(Titre, ListeIntermediaire, SansDoublons), !.
+trierListe([Titre|[]], SansDoublons) :- append([Titre], [], SansDoublons).
+											
+											
+filterListe(Titre,ListeIntermediaire,SansDoublons) :- \+member(Titre, ListeIntermediaire),
+													append([Titre], ListeIntermediaire, SansDoublons).
+filterListe(_,ListeIntermediaire,SansDoublons):- append([], ListeIntermediaire, SansDoublons).													
 
 
 
